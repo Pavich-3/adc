98d4fa550569a825b99928115c71f825bfc2d1c1
Pavlo Baranchuk <pavlo.baranchuk@goreel.tech>
2025-09-20 14:25:07 +0300

    added timer with 50hz interrupt

--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-#define F_CPU 16000000UL
+#define F_CPU 8000000UL
 
 void ADC_Init(void);
+void TIMER_Init(void);
 
 int main(void)
 {
@@ -13,8 +14,9 @@ int main(void)
 void ADC_Init(void)
 {
   ADMUX |= (1 << REFS0) | (1 << MUX0);
-  ADCSRA |= (1 << ADEN) | (1 << ADATE) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+  ADCSRA |= (1 << ADEN) | (1 << ADATE) | (1 << ADPS2) | (1 << ADPS1);
   ADCSRB |= (1 << ADTS1) | (1 << ADTS0);
+  DIDR0 |= (1 << ADC1D);
 }
 
 void PWM_Init(void)
@@ -24,5 +26,13 @@ void PWM_Init(void)
 
 void TIMER_Init(void)
 {
-  
+  TCCR0A |= (1 << WGM01);
+  TCCR0B |= (1 << CS02) | (1 << CS00);
+  OCR0A = 156;
+  TIMSK0 |= (1 << OCIE0A);
+}
+
+ISR(TIMER0_COMPA_vect)
+{
+
 }
